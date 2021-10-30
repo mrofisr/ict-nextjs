d2f72ea9dc6bfa311a32f983416a767493250e7c
mrofisr <rofim58@gmail.com>
2021-10-30 21:29:31 +0700

    Auth0 Testing

--- pages/index.js
+++ pages/index.js
@@ -1,74 +1,18 @@
-// Import the functions you need from the SDKs you need
-import React, { useEffect, useState } from "react";
-import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
-import firebase from "firebase/app";
-import "firebase/auth";
-import "firebase/firestore";
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyB4IyDokG4HF9kUmX6clj-TqpiCpFU2-GI",
-  authDomain: "petspace-1ebb6.firebaseapp.com",
-  projectId: "petspace-1ebb6",
-  storageBucket: "petspace-1ebb6.appspot.com",
-  messagingSenderId: "396664855565",
-  appId: "1:396664855565:web:d83c46fad732ca70308944",
-};
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-} else {
-  firebase.app(); // if already initialized, use that one
-}
-// Configure FirebaseUI.
-const uiConfig = {
-  // Popup signin flow rather than redirect flow.
-  signInFlow: "popup",
-  // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
-  signInSuccessUrl: "/home",
-  // We will display Google and Facebook as auth providers.
-  signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
-  callbacks: {
-    // Avoid redirects after sign-in.
-    signInSuccessWithAuthResult: () => false,
-  },
-};
-
-// Initialize Firebase
+import { useUser } from '@auth0/nextjs-auth0';
 
 export default function Home() {
-  const [isSignedIn, setIsSignedIn] = useState(false); // Local signed-in state.
+  const { user, error, isLoading } = useUser();
 
-  // Listen to the Firebase Auth state and set the local state.
-  useEffect(() => {
-    const unregisterAuthObserver = firebase
-      .auth()
-      .onAuthStateChanged((user) => {
-        setIsSignedIn(!!user);
-      });
-    return () => unregisterAuthObserver(); // Make sure we un-register Firebase observers when the component unmounts.
-  }, []);
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
 
-  if (!isSignedIn) {
+  if (user) {
     return (
       <div>
-        <StyledFirebaseAuth
-          uiConfig={uiConfig}
-          firebaseAuth={firebase.auth()}
-        />
+        Welcome {user.name}! <a href="/api/auth/logout">Logout</a>
       </div>
     );
   }
-  return (
-    <div>
-      <h1>My App</h1>
-      <p>
-        Welcome {firebase.auth().currentUser.displayName}! You are now
-        signed-in!
-      </p>
-      <button onClick={() => firebase.auth().signOut()}>Sign-out</button>
-    </div>
-  );
+
+  return <a href="/api/auth/login">Login</a>;
 }
