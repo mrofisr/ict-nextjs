abed902c7874ef73dd085bdebfa27d2e0e7de29d
Ivanrizkys <71715895+Ivanrizkys@users.noreply.github.com>
2021-11-03 22:05:43 +0700

    Refactoring code to handle request containing file

--- libs/get-req.js
+++ libs/get-req.js
@@ -1,14 +1,30 @@
 import formidable from "formidable";
+// const fs = require('fs');
+// export default function getRequest (request, path) {
+//     return new Promise((resolve, reject) => {
+//         const form = formidable({
+//             keepExtensions: true, 
+//             uploadDir: path,
+//             fileWriteStreamHandler: () =>
+//                 fs.createWriteStream('C:\\Users\\ivnri\\Documents\\ict\\ict-nextjs-1\\public')
 
-export default function getRequest (request, path) {
+//         });
+//         form.parse(request , (err, fields, files) => {
+//             if (err) return reject(err);
+//             return resolve({fields, files});
+//         })
+//     })
+// }
+
+export default function getRequest (req, path) {
     return new Promise((resolve, reject) => {
-        const form = formidable({
-            keepExtensions: true, 
-            uploadDir: path,
-        });
-        form.parse(request , (err, fields, files) => {
+        const form = new formidable.IncomingForm();
+        form.keepExtensions = true;
+        form.uploadDir = path;
+
+        form.parse (req, (err, fields, files) => {
             if (err) return reject(err);
-            return resolve({fields, files});
-        })
-    })
+            resolve({fields, files});
+        });
+    });
 }
