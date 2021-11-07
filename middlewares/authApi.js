de5d6974655ba5a24ddbed61dcf99819c2cff44a
Ivanrizkys <71715895+Ivanrizkys@users.noreply.github.com>
2021-11-07 20:56:05 +0700

    Adding new authorization to handle all user

--- middlewares/authApi.js
+++ middlewares/authApi.js
@@ -1,24 +1,48 @@
 import jwt from "jsonwebtoken";
 import { error } from "@/utils/res";
 
-export function authApi (req, res, role) {
-    return new Promise((resolve) => {
-        const {authorization} = req.headers;
-        const splitToken = authorization.split(" ");
-        const [tokenType, token] = splitToken;
-        if (tokenType !== 'Bearer') return res.status(401).json({
-            ...error,
-            error_status: 401,
-            message: "Token type not allowed"
-        });
-        jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-            if (err || decoded.role !== role) return res.status(401).json({
-                ...error,
-                error_status: 401,
-                message: "Token is not valid"
-            });
-            return resolve (decoded);
-        });
-    
-    })
+export function authApi(req, res, role) {
+	return new Promise((resolve) => {
+		const { authorization } = req.headers;
+		const splitToken = authorization.split(" ");
+		const [tokenType, token] = splitToken;
+		if (tokenType !== "Bearer")
+			return res.status(401).json({
+				...error,
+				error_status: 401,
+				message: "Token type not allowed",
+			});
+		jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+			if (err || decoded.role !== role)
+				return res.status(401).json({
+					...error,
+					error_status: 401,
+					message: "Token is not valid",
+				});
+			return resolve(decoded);
+		});
+	});
+}
+
+export function defaultAuthApi(req, res) {
+	return new Promise((resolve) => {
+		const { authorization } = req.headers;
+		const splitToken = authorization.split(" ");
+		const [tokenType, token] = splitToken;
+		if (tokenType !== "Bearer")
+			return res.status(401).json({
+				...error,
+				error_status: 401,
+				message: "Token type not allowed",
+			});
+		jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+			if (err)
+				return res.status(401).json({
+					...error,
+					error_status: 401,
+					message: "Token is not valid",
+				});
+			return resolve(decoded);
+		});
+	});
 }
