4eae07dfadabfad72fd33f9bc977106907919d55
lintangpratama <lintangajiyogapratama@gmail.com>
2021-11-12 23:28:53 +0700

    finishing login and register page

--- pages/account/login.js
+++ pages/account/login.js
@@ -4,75 +4,92 @@ import Link from "next/link";
 import { useState } from "react";
 import Cookies from "js-cookie";
 import { unAuthPage } from "@/middlewares/auth-page-user";
-import Router  from "next/router";
+import Router from "next/router";
 
-export async function getServerSideProps (ctx) {
-  const {token_user, token_user_penitipan} = await unAuthPage(ctx);
-  if (token_user) 
+export async function getServerSideProps(ctx) {
+  const { token_user, token_user_penitipan } = await unAuthPage(ctx);
+  if (token_user)
     return {
       redirect: {
         permanent: false,
-        destination: '/',
+        destination: "/",
       },
-    }
+    };
   if (token_user_penitipan)
     return {
       redirect: {
         permanent: false,
-        destination: '/petcare/space',
+        destination: "/petcare/space",
       },
-    }
+    };
 
   return {
-    props: {}
-  }
+    props: {},
+  };
 }
 
 export default function Login() {
   const [fields, setFields] = useState({
     email: "",
-    password: ""
+    password: "",
   });
 
-  async function loginUser (e) {
+  async function loginUser(e) {
     e.preventDefault();
-    const login = await fetch('/api/user/auth/login?role=user', {
+    const login = await fetch("/api/user/auth/login?role=user", {
       method: "POST",
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(fields)
+      body: JSON.stringify(fields),
     });
     const loginRes = await login.json();
     if (!login.ok) return console.log(`error ${loginRes.message}`);
     Cookies.set("user_cookie", loginRes.data.token);
-    Router.push('/')
+    Router.push("/");
   }
 
-  async function loginUserPenitipan (e) {
+  async function loginUserPenitipan(e) {
     e.preventDefault();
-    const login = await fetch('/api/user/auth/login?role=user_penitipan', {
+    const login = await fetch("/api/user/auth/login?role=user_penitipan", {
       method: "POST",
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(fields)
+      body: JSON.stringify(fields),
     });
     const loginRes = await login.json();
-    if (!login.ok) return console.log(`error, ${loginRes.message}`);
-    console.log(loginRes.data.token);
+    if (!login.ok) {
+      document.getElementById("modal-failed").classList.remove("hidden");
+      return;
+    }
     Cookies.set("user_penitipan_cookie", loginRes.data.token);
-    Router.push('/petcare/space');
+
+    if (loginRes.succes) {
+      document.getElementById("modal-acc").classList.remove("hidden");
+      return;
+    }
+
+    
   }
 
-  function fieldHandler (e) {
-    const getName = e.target.getAttribute('name');
-    
+  function fieldHandler(e) {
+    const getName = e.target.getAttribute("name");
+
     setFields({
       ...fields,
-      [getName]: e.target.value
-    })
+      [getName]: e.target.value,
+    });
   }
+
+  const okButtonHandler = (e) => {
+    location.href = "/petcare/space";
+  };
+
+  const failButtonHandler = (e) => {
+    document.getElementById("modal-failed").classList.add("hidden");
+  };
+
   return (
     <div className="flex flex-col">
       <Head />
@@ -115,7 +132,11 @@ export default function Login() {
                       name="email"
                       className="py-2 text-sm bg-background bg-opacity-40 text-search-font border-blue-secondary text-form rounded-md pl-10 focus:outline-none focus:ring focus:border-blue-50 bg-abu border-abu border-2"
                       autoComplete="off"
-                      style={{ width: "100%", textIndent: "24px", height: "47px" }}
+                      style={{
+                        width: "100%",
+                        textIndent: "24px",
+                        height: "47px",
+                      }}
                     ></input>
                   </div>
                   {/* Masukkan Password */}
@@ -135,10 +156,14 @@ export default function Login() {
                       name="password"
                       className="py-2 text-sm text-form bg-background text-search-font border-blue-secondary bg-opacity-40 rounded-md pl-10 focus:outline-none focus:ring focus:border-blue-50 bg-abu h-md border-abu border-2"
                       autoComplete="off"
-                      style={{ width: "100%", textIndent: "24px", height: "47px" }}
+                      style={{
+                        width: "100%",
+                        textIndent: "24px",
+                        height: "47px",
+                      }}
                     ></input>
                   </div>
-                  
+
                   {/* Button Sign */}
                   <input
                     onClick={loginUser}
@@ -154,26 +179,111 @@ export default function Login() {
                   ></input>
                   {/*  */}
                   <div className="text-center mt-5 text-sm">
-                    
+                    <div
+                      id="modal-acc"
+                      className="max-w-md -ml-5 w-full fixed bottom-40 hidden"
+                    >
+                      <div className="mx-auto p-5 border w-5/6 shadow-lg rounded-md bg-white">
+                        <div className="mt-3 text-center">
+                          <div className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-green-100">
+                            <svg
+                              className="h-6 w-6 text-green-600"
+                              fill="none"
+                              stroke="currentColor"
+                              viewBox="0 0 24 24"
+                              xmlns="http://www.w3.org/2000/svg"
+                            >
+                              <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M5 13l4 4L19 7"
+                              ></path>
+                            </svg>
+                          </div>
+                          <h3 className="text-lg leading-6 font-medium text-gray-900">
+                            Successful!
+                          </h3>
+                          <div className="mt-2 px-7 py-3">
+                            <p className="text-sm text-gray-500">
+                              Account has been successfully registered!
+                            </p>
+                          </div>
+                          <div className="items-center px-4 py-3">
+                            <button
+                              id="ok-btn"
+                              onClick={okButtonHandler}
+                              className="px-4 py-2 bg-green-500 text-white text-base font-medium rounded-md w-full shadow-sm hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-300"
+                            >
+                              OK
+                            </button>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+
+                    {/* modal failed */}
+                    <div
+                      id="modal-failed"
+                      className="max-w-md -ml-5 w-full fixed bottom-40 hidden"
+                    >
+                      <div className="mx-auto p-5 border w-5/6 shadow-lg rounded-md bg-white">
+                        <div className="mt-3 text-center">
+                          <div className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-red-100">
+                            <svg
+                              aria-hidden="true"
+                              focusable="false"
+                              data-prefix="fas"
+                              data-icon="times"
+                              className="svg-inline--fa fa-times fa-w-11 h-6 w-6 "
+                              role="img"
+                              xmlns="http://www.w3.org/2000/svg"
+                              viewBox="0 0 352 512"
+                            >
+                              <path
+                                fill="#FF0000"
+                                d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"
+                              ></path>
+                            </svg>
+                          </div>
+                          <h3 className="text-lg leading-6 font-medium text-gray-900">
+                            Failed!
+                          </h3>
+                          <div className="mt-2 px-7 py-3">
+                            <p className="text-sm text-gray-500">
+                              Account failed to registered!
+                            </p>
+                          </div>
+                          <div className="items-center px-4 py-3">
+                            <button
+                              id="fail-btn"
+                              onClick={failButtonHandler}
+                              className="px-4 py-2 bg-red-500 text-white text-base font-medium rounded-md w-full shadow-sm hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-green-300"
+                            >
+                              OK
+                            </button>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
 
                     {/*  */}
                     <Link href="/account/signup">
-                    <p className="mt-5 text-sm" style={{ color: "#7C7C80" }}>
-                      Belum punya akun?{" "}
-                      <a>
-                        <span
-                          className="font-semibold text-md cursor-pointer hover:underline"
-                          style={{ color: "#429FF2" }}
-                        >
-                          Daftar di sini
-                        </span>
-                      </a>
-                    </p>
+                      <p className="mt-5 text-sm" style={{ color: "#7C7C80" }}>
+                        Belum punya akun?{" "}
+                        <a>
+                          <span
+                            className="font-semibold text-md cursor-pointer hover:underline"
+                            style={{ color: "#429FF2" }}
+                          >
+                            Daftar di sini
+                          </span>
+                        </a>
+                      </p>
                     </Link>
                   </div>
                 </form>
                 <div className="w-full h-96"></div>
-              
               </div>
             </div>
           </div>
